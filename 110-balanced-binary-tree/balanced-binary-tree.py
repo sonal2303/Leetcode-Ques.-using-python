742f1afd7f179f3e6806a1bd38f363b69c5354d7
sonal2303 <sonalkumarisona2303@gmail.com>
2026-09-19 16:04:23 +0530

    Time: 11 ms (20.01%) | Memory: 18.1 MB (25.28%) - LeetSync

---
+++ 110-balanced-binary-tree/balanced-binary-tree.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isBalanced(self, root):
+        def height(node):
+            if node is None:
+                return 0
+
+            left = height(node.left)
+            right = height(node.right)
+
+            if left == -1 or right == -1:
+                return -1
+
+            if abs(left - right) > 1:
+                return -1
+
+            return max(left, right) + 1
+
+        return height(root) != -1
